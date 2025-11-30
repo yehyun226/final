e368debed8afd5ca5e8dd90f1cb19d971d08e10b
yehyun226 <1361015syh226@gmail.com>
2025-11-30 22:17:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ def login_screen():
                 "role": user["role"],
             }
             st.success("로그인 성공!")
-            st.experimental_rerun()
+            st.rerun()
         else:
             st.error("비밀번호가 올바르지 않습니다.")
 
